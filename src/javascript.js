69a0f27b42283e7be758ee81ff3a5664b748f969
chlolobxo <159199669+chlolobxo@users.noreply.github.com>
2024-02-10 14:02:45 +0000

    Tried to add the API for the temperature
    It doesn't work, so doing some debugging currently

--- src/javascript.js
+++ src/javascript.js
@@ -13,5 +13,13 @@ function handleSearch(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
 
+function displayTemperature(response) {
+  let temperature = document.querySelector("temperature");
+  let temperatureResponse = Math.round(response.data.temperature.current);
+  temperature.innerHTML = temperatureResponse;
+}
+
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 let apiKey = "3cob132dc3daat1dec233c5354fa90b7";
+
+axios.get(apiUrl).then(displayTemperature);
