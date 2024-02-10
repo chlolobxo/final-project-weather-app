d9166742f66ae35f88ba46811d54efa1bb254500
chlolobxo <159199669+chlolobxo@users.noreply.github.com>
2024-02-10 14:41:32 +0000

    Cleaned up the search engine API and sorted the fixes

--- src/javascript.js
+++ src/javascript.js
@@ -1,25 +1,29 @@
+function refreshWeather(response) {
+  let temperature = document.querySelector("#temperature");
+  let temperatureResponse = Math.round(response.data.temperature.current);
+  let h1 = document.querySelector("h1");
+
+  h1.innerHTML = response.data.city;
+  temperature.innerHTML = temperatureResponse;
+}
+
+function searchCity(city) {
+  let apiKey = "3cob132dc3daat1dec233c5354fa90b7";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-text-input");
-  let h1 = document.querySelector("h1");
 
   if (searchInput.value === "") {
     alert("Please enter a city");
-  } else {
-    h1.innerHTML = searchInput.value;
   }
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
 
-function displayTemperature(response) {
-  let temperature = document.querySelector("temperature");
-  let temperatureResponse = Math.round(response.data.temperature.current);
-  temperature.innerHTML = temperatureResponse;
-}
-
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-let apiKey = "3cob132dc3daat1dec233c5354fa90b7";
-
-axios.get(apiUrl).then(displayTemperature);
+searchCity("Glasgow");
